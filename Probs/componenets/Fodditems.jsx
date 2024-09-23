e17e2ed35017c25bbf40c13397eeaeea9a3c90c7
CoderRaushan <raushankumar23082004@gmail.com>
2024-09-23 21:07:45 +0530

    react

--- Probs/componenets/Fodditems.jsx
+++ Probs/componenets/Fodditems.jsx
@@ -1,12 +1,16 @@
 import Item from "./Item";
-
+const clickedbuybutton=(fooditems)=>
+    {
+    
+    }
 function Fooditems({fooditems}) {
+   
     return (
         <ul className="list-group" >
             {
                 fooditems.map((item) =>
                 (
-                   <Item key={item} item={item} />
+                   <Item key={item} item={item}  clickedbuybutton={()=>console.log(`${item} is clicked!`)}/>
                 ))
             }
         </ul>
--- Probs/componenets/Item.jsx
+++ Probs/componenets/Item.jsx
@@ -1,7 +1,12 @@
 import styles from "../componenets/Item.module.css";// file.module.css
+
 const Item=(probs)=>
 {
-   return <li className={`${styles['ul_color_cl']} list-group-item`}>{probs.item}</li>;
+   return <li className={`${styles['ul_color_cl']} list-group-item`}>{probs.item}
+    <button className={`${styles['but_btn']} btn btn-info`}
+              onClick={probs.clickedbuybutton}
+              >Buy</button>
+   </li>
 }
 export default Item;
 
