4823b5c94759793e5203b8b72527dfcfa66b1119
CoderRaushan <raushankumar23082004@gmail.com>
2024-10-12 18:19:45 +0530

    sm

---
+++ SocialMedia/src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Components/Footer"
+import Header from "./Components/Header"
+import Sidebar from "./Components/Sidebar"
+import "./App.css"
+function App() 
+{
+  return (
+    <>
+      <Header/>
+      <Sidebar/> 
+      <Footer/>
+    </>
+  )
+}
+
+export default App
