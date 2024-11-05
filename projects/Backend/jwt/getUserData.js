a7e72341756270f616f9698b193a8122e663a5ad
CoderRaushan <raushankumar23082004@gmail.com>
2024-11-05 15:38:51 +0530

    react

--- projects/Backend/jwt/getUserData.js
+++ projects/Backend/jwt/getUserData.js
@@ -4,7 +4,7 @@ export const getUserData = (req, res) =>
   const token = req.cookies.jwt; 
   if (!token) 
   {
-    console.log("Token verification error: No token provided",token);
+    // console.log("Token verification error: No token provided",token);  
     return res.status(401).json({ error: "No token provided" });
   }  
   jwt.verify(token, process.env.JwtTokenKEY, (err, decoded) => {
